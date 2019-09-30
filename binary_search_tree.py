9197e9bdecf59322219bbcb5dcb8d6f1c4685d68
Brandi Williams <brandimichelle.0911@gmail.com>
2019-09-30 15:01:43 -0700

    Create binary_search_tree.py

---
+++ binary_search_tree.py
@@ -0,0 +1,28 @@
+class BinaryNode:
+    def __init__():
+      # a node has data, left, and right pointers
+      # left and right are intialized as null
+    
+
+class BinaryTree:
+    def __init__():
+        # when a new Tree is made, it has a root property
+    
+    def insert(self, data):
+        # add a new Node to the tree, with data as the Node's data
+        # if the data is already in the tree, do not insert it
+    
+    def search(self, val):
+        # search the Tree for a node with the given value
+        # if the node exists, return it
+        # if the node doesn't exist, return false
+    
+    size(self, node):
+        # calculate the number of nodes in the tree, starting from the given node
+    
+    getMax(self):
+        # return the maximum value stored in the tree
+    
+    def height(self, node):
+        # calculate the maximum amount of nodes in any one path from the given node
+        # if not given a specific node, default to using the root node
